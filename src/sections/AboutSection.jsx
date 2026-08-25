002139bb712ce3ef0cda81212223f9edd54037c9
MalaikaFayyaz <0915malaika@gmail.com>
2026-08-25 15:33:40 +0500

    about updated

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -8,7 +8,7 @@ function AboutSection() {
         <h1 className="pf-title">a bit more <span className="accent">context</span>.</h1>
         <div className="about-columns">
           <p className="pf-desc">
-           Juggling between coursework, TAship, and internship, I am trying to survive. Alongside, I am building a product based on solving an issue faced by local commerce platforms. Thus, the current work stack is MERN. Besides this, When I am busy procastinating, I love to build out of my lexicon and be nerdy about computer architecture. 
+           Juggling between coursework, TAship, and internship manifestation, I am trying to survive. Alongside, I am building a product based on solving an issue faced by local commerce platforms. Thus, the current work stack is MERN. Besides this, When I am busy procastinating, I love to build out of my lexicon and be nerdy about computer architecture. 
           </p>
           <ul className="pf-list">
             <li><span>&gt;</span> Based in Sol 3.</li>
